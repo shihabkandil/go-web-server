580b6ef7d2eb09aa5b7d8f396c5a503e07177228
Shihab Kandil <shihab.mostafa1@gmail.com>
2024-08-26 01:13:44 +0300

    feat: Implement basic TCP server to handle HTTP GET requests

---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
+
+func main() {
+
+	listener, err := net.Listen("tcp", "localhost:8080")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Listening...")
+
+	defer listener.Close()
+
+	handleConnection(listener)
+
+}
+func handleConnection(listener net.Listener) {
+	connection, err := listener.Accept()
+
+	if err != nil {
+		log.Fatalf("Failed to start TCP listener: %v", err)
+	}
+
+	buffer := make([]byte, 1024)
+
+	_, readError := connection.Read(buffer)
+
+	if readError != nil {
+		log.Fatalf("Failed to read connection buffer: %v", readError)
+	}
+
+	bufferText := string(buffer[:])
+
+	fields := strings.Fields(bufferText)
+
+	if len(fields) > 0 {
+		method := fields[0]
+		if method == "GET" {
+			handleGetCall(fields, connection)
+		}
+	}
+}
+
+func handleGetCall(fields []string, conn net.Conn) {
+	defer conn.Close()
+
+	if len(fields) > 2 {
+		requestedRescource := fields[1]
+
+		if requestedRescource == "/" {
+			conn.Write([]byte("HTTP/1.1 200 OK\r\n\r\n Hello World!"))
+		} else {
+			conn.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+		}
+	}
+}
